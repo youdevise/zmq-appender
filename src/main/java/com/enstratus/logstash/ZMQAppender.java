898213c2b3e8f32661ca348c3ab0cc823df984d7
ahepburn <ashton.hepburn@timgroup.com>
2012-09-07 11:34:00 -0400

    Revert "Ash/chris - initial changes to support logback instead of log4j"
    This reverts commit da0525439ca8800005fee75e1a674b6219f52a39.

--- src/main/java/com/enstratus/logstash/ZMQAppender.java
+++ src/main/java/com/enstratus/logstash/ZMQAppender.java
@@ -1,140 +1,84 @@
 package com.enstratus.logstash;
 
-import ch.qos.logback.classic.encoder.PatternLayoutEncoder;
-import ch.qos.logback.classic.spi.ILoggingEvent;
-import ch.qos.logback.core.AppenderBase;
-import com.enstratus.logstash.data.LoggingEventData;
-import com.enstratus.logstash.layouts.JSONMessage;
-import com.enstratus.logstash.layouts.LogstashMessage;
-import com.google.gson.Gson;
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+import org.apache.log4j.helpers.LogLog;
 import org.zeromq.ZMQ;
+import org.zeromq.ZMQ.Context;
 import org.zeromq.ZMQ.Socket;
 
-import java.io.IOException;
+import com.google.gson.*;
 
-public class ZMQAppender extends AppenderBase<ILoggingEvent> {
-    private static final String PUBSUB = "pub";
-    private static final String PUSHPULL = "push";
-    private static final String CONNECTMODE = "connect";
-    private static final String BINDMODE = "bind";
+import com.enstratus.logstash.data.LoggingEventData;
+import com.enstratus.logstash.layouts.*;
+
+public class ZMQAppender extends AppenderSkeleton {
+
+	private Socket socket;
+	private final Gson gson = new Gson();
+
+	// ZMQ specific "stuff"
+	private int threads;
+	private int hwm;
+	private String endpoint;
+	private String mode;
+	private String socketType;
+	private String topic;
+	private String identity;
+	private boolean blocking;
+	
+	// Ancillary settings
+	private String tags;
+	private String eventFormat = "json_event";
+	
+	private static final String PUBSUB = "pub";
+	private static final String PUSHPULL = "push";
+	private static final String CONNECTMODE = "connect";
+	private static final String BINDMODE = "bind";
     private static final String JSONFORMAT = "json";
     private static final String JSONEVENTFORMAT = "json_event";
 
-    private Socket socket;
-    private final Gson gson = new Gson();
+	public ZMQAppender() {
+		super();
+	}
 
-    private PatternLayoutEncoder encoder;
+	public ZMQAppender(final Socket socket) {
+		this();
+		this.socket = socket;
+	}
 
+	@Override
+	public void close() {
 
-    // ZMQ specific "stuff"
-    private int threads;
-    private int hwm;
-    private String endpoint;
-    private String mode;
-    private String socketType;
-    private String topic;
-    private String identity;
-    private boolean blocking;
+	}
 
-    // Ancillary settings
-    private String tags;
-    private String eventFormat = "json_event";
+	@Override
+	public boolean requiresLayout() {
+        return false;
+	}
 
-
-    public ZMQAppender() {
-        super();
-    }
-
-    public ZMQAppender(final Socket socket) {
-        this();
-        this.socket = socket;
-    }
-
-    @Override
-    public void start() {
-        if (this.encoder == null) {
-            addError("No layout set for the appender named [" + name + "].");
-            return;
-        }
-        super.start();
-
-
-        final ZMQ.Context context = ZMQ.context(threads);
-        Socket sender;
-
-        if (PUBSUB.equals(socketType)) {
-            LogLog.debug("Setting socket type to PUB");
-            sender = context.socket(ZMQ.PUB);
-        } else if (PUSHPULL.equals(socketType)) {
-            LogLog.debug("Setting socket type to PUSH");
-            sender = context.socket(ZMQ.PUSH);
-        } else {
-            LogLog.debug("Setting socket type to default PUB");
-            sender = context.socket(ZMQ.PUB);
-        }
-        sender.setLinger(1);
-
-        final Socket socket = sender;
-
-        final String[] endpoints = endpoint.split(",");
-
-        for (String ep : endpoints) {
-
-            if (BINDMODE.equals(mode)) {
-                LogLog.debug("Binding socket to " + ep);
-                socket.bind(ep);
-            } else if (CONNECTMODE.equals(mode)) {
-                LogLog.debug("Connecting socket to " + ep);
-                socket.connect(ep);
-            } else {
-                LogLog.debug("Default connecting socket to " + ep);
-                socket.connect(ep);
-            }
-        }
-
-        if (identity != null) {
-            socket.setIdentity(identity.getBytes());
-        }
-
-        this.socket = socket;
-
-    }
-
-    @Override
-    protected void append(ILoggingEvent loggingevent) {
-        // note that AppenderBase.doAppend will invoke this method only if
-        // this appender was successfully started.
-
-        String s = "";
-
-        try {
-            this.encoder.doEncode(loggingevent);
-        } catch (IOException e) {
-            // we can't do much with the exception except halting
-            super.stop();
-            addError("Failed to write to the console");
-        }
-
-        System.out.println(s);
-
-        //code from old log4j implmentation
-        final LoggingEventData data = new LoggingEventData(loggingevent);
+	@Override
+	protected void append(LoggingEvent event) {
+		final LoggingEventData data = new LoggingEventData(event);
         String messageFormat = getEventFormat();
         String logLine = "";
 
         String identifier = getIdentity();
         String[] tagz;
-        if (!(null == tags)) {
+        if(!(null == tags)) {
             tagz = getTags().split(",");
-        } else {
+        }
+        else
+        {
             tagz = null;
         }
 
-        if (JSONFORMAT.equals(messageFormat)) {
-            JSONMessage message = new JSONMessage(data, identifier, tagz);
+        if(JSONFORMAT.equals(messageFormat)) {
+            JSONMessage message = new JSONMessage(data,identifier,tagz);
             logLine = message.toJson();
-        } else if (JSONEVENTFORMAT.equals(messageFormat)) {
-            LogstashMessage message = new LogstashMessage(data, identifier, tagz);
+        }
+        else if(JSONEVENTFORMAT.equals(messageFormat)) {
+            LogstashMessage message = new LogstashMessage(data,identifier,tagz);
             logLine = message.toJson();
         }
         if ((topic != null) && (PUBSUB.equals(socketType))) {
@@ -142,86 +86,129 @@ public class ZMQAppender extends AppenderBase<ILoggingEvent> {
         }
 
         socket.send(logLine.getBytes(), blocking ? 0 : ZMQ.NOBLOCK);
-
     }
 
-    public PatternLayoutEncoder getEncoder() {
-        return encoder;
-    }
+	@Override
+	public void activateOptions() {
+		super.activateOptions();
 
-    public void setEncoder(PatternLayoutEncoder encoder) {
-        this.encoder = encoder;
-    }
+		final Context context = ZMQ.context(threads);
+		Socket sender;
 
-    public int getThreads() {
-        return threads;
-    }
+		if (PUBSUB.equals(socketType)) {
+			LogLog.debug("Setting socket type to PUB");
+			sender = context.socket(ZMQ.PUB);
+		}
+		else if (PUSHPULL.equals(socketType))
+		{
+			LogLog.debug("Setting socket type to PUSH");
+			sender = context.socket(ZMQ.PUSH);
+		}
+		else
+		{
+			LogLog.debug("Setting socket type to default PUB");
+			sender = context.socket(ZMQ.PUB);
+		}
+		sender.setLinger(1);
+		
+		final Socket socket = sender;
+		
+		final String[] endpoints = endpoint.split(",");
+		
+		for(String ep : endpoints) {
+			
+			if (BINDMODE.equals(mode)) {
+				LogLog.debug("Binding socket to " + ep);
+				socket.bind(ep);
+			}
+			else if (CONNECTMODE.equals(mode))
+			{
+				LogLog.debug("Connecting socket to " + ep);
+				socket.connect(ep);
+			}
+			else
+			{
+				LogLog.debug("Default connecting socket to " + ep);
+				socket.connect(ep);
+			}	
+		}
+		
+		if (identity != null) {
+			socket.setIdentity(identity.getBytes());
+		}
+		
+		this.socket = socket;
+	}
 
-    public void setThreads(final int threads) {
-        this.threads = threads;
-    }
+	public int getThreads() {
+		return threads;
+	}
 
-    public String getEndpoint() {
-        return endpoint;
-    }
+	public void setThreads(final int threads) {
+		this.threads = threads;
+	}
 
-    public void setEndpoint(final String endpoint) {
-        this.endpoint = endpoint;
-    }
+	public String getEndpoint() {
+		return endpoint;
+	}
 
-    public boolean isBlocking() {
-        return blocking;
-    }
+	public void setEndpoint(final String endpoint) {
+		this.endpoint = endpoint;
+	}
 
-    public void setBlocking(boolean blocking) {
-        this.blocking = blocking;
-    }
+	public boolean isBlocking() {
+		return blocking;
+	}
 
-    public String getTopic() {
-        return topic;
-    }
+	public void setBlocking(boolean blocking) {
+		this.blocking = blocking;
+	}
+	
+	public String getTopic() {
+		return topic;
+	}
+	
+	public void setTopic(final String topic) {
+		this.topic = topic;
+	}
+	
+	public String getSocketType() {
+		return socketType;
+	}
+	
+	public void setSocketType(final String socketType) {
+		this.socketType = socketType;
+	}
+	
+	public String getMode() {
+		return mode;
+	}
+	
+	public void setMode(final String mode) {
+		this.mode = mode;
+	}
+	
+	public String getIdentity() {
+		return identity;
+	}
+	
+	public void setIdentity(final String identity) {
+		this.identity = identity;
+	}
+	
+	public String getTags() {
+		return tags;
+	}
+	
+	public void setTags(final String tags) {
+		this.tags = tags;
+	}
+	
+	public String getEventFormat() {
+		return eventFormat;
+	}
 
-    public void setTopic(final String topic) {
-        this.topic = topic;
-    }
-
-    public String getSocketType() {
-        return socketType;
-    }
-
-    public void setSocketType(final String socketType) {
-        this.socketType = socketType;
-    }
-
-    public String getMode() {
-        return mode;
-    }
-
-    public void setMode(final String mode) {
-        this.mode = mode;
-    }
-
-    public String getIdentity() {
-        return identity;
-    }
-
-    public void setIdentity(final String identity) {
-        this.identity = identity;
-    }
-
-    public String getTags() {
-        return tags;
-    }
-
-    public void setTags(final String tags) {
-        this.tags = tags;
-    }
-
-    public String getEventFormat() {
-        return eventFormat;
-    }
-
-    public void setEventFormat(final String eventFormat) {
-        this.eventFormat = eventFormat;
-    }
+	public void setEventFormat(final String eventFormat) {
+		this.eventFormat = eventFormat;
+	}
 }
